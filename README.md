d4245363c46271e478202122a6c5aedffe0dc07c
sjllls <2012ji123@gmail.com>
2020-08-27 10:17:24 +0800

    kagura: Intialization

---
+++ README.md
@@ -0,0 +1,28 @@
+Copyright (C) 2016 - The CyanogenMod Project
+
+Copyright (C) 2017-2020 - The LineageOS Project
+
+Device configuration for Sony Xperia XZ
+==============
+
+The Sony Xperia XZ (codenamed _"kagura"_) is a high-end smartphone from Sony in 2016.
+
+It was released date was Oct 2016.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+SoC     | Qualcomm MSM8996 Snapdragon 820
+CPU     | Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo)
+GPU     | Adreno 530
+Memory  | 3 GB RAM
+Shipped Android Version | 6.0, upgradable to 8.0
+Storage | 32/64 GB
+Battery | Non-removable Li-Ion 2900 mAh battery
+Display | 1080 x 1920 pixels, 16:9 ratio (~424 ppi density)
+Camera  | 23 MP, f/2.0, 24mm (wide), 1/2.3", predictive PDAF, Laser AF
+
+## Device picture
+
+![Sony Xperia XZs](https://fdn2.gsmarena.com/vv/pics/sony/sony-xperia-xz.jpg "Sony Xperia XZs")
---
+++ device.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/sony/kagura/kagura-vendor.mk)
+
+# Local overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# Inherit from tone-common
+$(call inherit-product, device/sony/tone-common/tone.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
+
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.target.rc
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/clearpad.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/clearpad.idc
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
